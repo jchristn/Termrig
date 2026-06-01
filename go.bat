018717f9b6f3e5ba403212cf0fa33b0960506c72
Joel Christner <joel.christner@gmail.com>
2026-06-01 16:29:21 -0700

    Build before launching app scripts

--- go.bat
+++ go.bat
@@ -1,1 +1,5 @@
-@dotnet run --project src\Termrig.App\Termrig.App.csproj
+@echo off
+cd src
+dotnet build
+cd ..
+dotnet run --project src\Termrig.App\Termrig.App.csproj
--- go.sh
+++ go.sh
@@ -4,4 +4,7 @@ set -eu
 SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 cd "$SCRIPT_DIR"
 
+cd src
+dotnet build
+cd ..
 dotnet run --project src/Termrig.App/Termrig.App.csproj
